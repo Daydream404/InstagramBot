6e88cf1d310e62470acd5cbc66dc6dd64e8bf6ea
Daydream404 <polishmaens@gmail.com>
2020-08-05 13:44:17 +0200

    now we can like 10 pictures

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 from selenium import webdriver
 from time import sleep
+import random
 
 username = open("D:\\Python\\DiscordPyIG\\ig_name.txt", "r").read()
 password = open("D:\\Python\\DiscordPyIG\\ig_pass.txt", "r").read()
 hashtag = '#pythonprogramming'
+number = 0
 
 driver = webdriver.Chrome('D:/Python/DiscordPyIG/chromedriver')
 
+def rndnum():
+    number = random.uniform(2.1,8.1)
+    sleep(number)
+
+
 def main(username, password):
     driver.get('https://instagram.com')
-    sleep(2)
+    rndnum()
     driver.find_element_by_xpath('//input[@name=\"username\"]').send_keys(username)
-    sleep(4)
+    rndnum()
     driver.find_element_by_xpath('//input[@name=\"password\"]').send_keys(password)
-    sleep(5)
+    rndnum()
     driver.find_element_by_xpath('//button[@type="submit"]').click()
-    sleep(6)
+    rndnum()
     driver.find_element_by_xpath('//button[contains(text(), "Not Now")]').click()
-    sleep(3)
+    rndnum()
     driver.find_element_by_xpath('//button[contains(text(), "Not Now")]').click()
-    sleep(4)
+    rndnum()
+
 
 def likes():
     driver.find_element_by_xpath("//input[@placeholder='Search']").send_keys(hashtag)
-    sleep(2)
+    rndnum()
     driver.find_element_by_class_name("z556c").click()
-    sleep(2)
+    rndnum()
     driver.find_element_by_class_name("eLAPa").click()
-    sleep(2.5)
-    driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div/div[3]/section[1]/span[1]/button').click()#like button
-    sleep(3)
-    driver.find_element_by_class_name("coreSpriteRightPaginationArrow").click()
-    sleep(2.3)
-    
+    rndnum()
+    for i in range(10):
+        driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div/div[3]/section[1]/span[1]/button').click()#like button
+        rndnum()
+        driver.find_element_by_class_name("coreSpriteRightPaginationArrow").click()
+        rndnum()
 
 
 def logout():
-    sleep(2.1)
+    rndnum()
     driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[3]/button').click()#settingsbtn
-    sleep(2.5)
+    rndnum()
     driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[3]/div/div/div[2]/div[2]/div').click()#logout button
-    sleep(2.3)
+    rndnum()
     driver.quit()
 
 
---
+++ rndnumb.py
@@ -0,0 +1,18 @@
+import random
+from time import sleep
+
+'''
+for i in range(10):
+    numb = random.uniform(2.1,8.1)
+    print(numb)
+
+sleep(numb)
+print("OK")
+'''
+
+numb = 0
+numb = random.uniform(2.1,8.1)
+print(numb)
+numb = 0
+numb = random.uniform(2.1,8.1)
+print(numb)
